00d5177eceb24b68c0a171ee660826da07c74da6
ko-taku <kosumgul@naver.com>
2025-04-04 17:13:01 +0900

    complete

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -1,1 +1,36 @@
-contract Calculator {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AbstractCalculator.sol";
+
+contract Calculator is AbstractCalculator {
+    function calculate(
+        uint256 a,
+        uint256 b,
+        string memory operation
+    ) public pure override returns (uint256) {
+        if (
+            keccak256(abi.encodePacked(operation)) ==
+            keccak256(abi.encodePacked("add"))
+        ) {
+            return add(a, b);
+        } else if (
+            keccak256(abi.encodePacked(operation)) ==
+            keccak256(abi.encodePacked("subtract"))
+        ) {
+            return subtract(a, b);
+        } else if (
+            keccak256(abi.encodePacked(operation)) ==
+            keccak256(abi.encodePacked("multiply"))
+        ) {
+            return multiply(a, b);
+        } else if (
+            keccak256(abi.encodePacked(operation)) ==
+            keccak256(abi.encodePacked("divide"))
+        ) {
+            return divide(a, b);
+        } else {
+            revert("Invalid operation");
+        }
+    }
+}
